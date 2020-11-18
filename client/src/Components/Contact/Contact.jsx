6e57791fb43e0c19f2d9689d3be3cc4b16fad6cf
Ashlyn Smith <ashlyn.marie.smith2@gmail.com>
2020-11-18 01:08:24 -0500

    updated class to className on Contact.js

--- client/src/Components/Contact/Contact.jsx
+++ client/src/Components/Contact/Contact.jsx
@@ -3,36 +3,36 @@ import "./Contact.css"
 
 function Contact() {
     return(
-      <section class="row-contact">
+      <section className="row-contact">
           <form>
-            <div class="form-group">
+            <div className="form-group">
               <label for="FormControlName">Name</label>
               <input
-                class="form-control"
+                className="form-control"
                 id="FormControlName"
                 placeholder="Name"
               />
             </div>
-            <div class="form-group">
+            <div className="form-group">
               <label for="exampleFormControlInput1">Email</label>
               <input
                 type="email"
-                class="form-control"
+                className="form-control"
                 id="exampleFormControlInput1"
                 placeholder="Email"
               />
             </div>
-            <div class="form-group">
+            <div className="form-group">
               <label for="exampleFormControlTextarea1">Message</label>
               <textarea
-                class="form-control"
+                className="form-control"
                 id="exampleFormControlTextarea1"
                 placeholder="Message"
                 rows="3"
               ></textarea>
             </div>
         </form>
-        <button type="submit" class="btn btn-primary btn-lg custom-lg rounded-80" >Submit</button>
+        <button type="submit" className="btn btn-primary btn-lg custom-lg rounded-80" >Submit</button>
           </section>
       )}
       export default Contact;
