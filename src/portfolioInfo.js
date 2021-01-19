4eaea1f184cb0f227f1950a045ea9ddf34845406
Ashlyn Smith <ashlyn.marie.smith2@gmail.com>
2021-01-18 19:14:11 -0500

    updated portfolio page

--- src/portfolioInfo.js
+++ src/portfolioInfo.js
@@ -2,46 +2,46 @@ const resourcesJSON =
 [
     {
       "id": 1,
-      "name": "Project One: The Date Night Planner",
-      "image": "https://ashlynimages.s3.us-east-2.amazonaws.com/Screen+Shot+2020-09-18+at+11.40.13+PM.png",
-      "github": "https://github.com/AshlynMarie22/The-Date-Night-Planner.git",
-      "website": "https://ashlynmarie22.github.io/The-Date-Night-Planner/"
+      "name": "Project Three: Link-Dyscovery",
+      "image": "https://user-images.githubusercontent.com/69767328/99280377-41433780-27ff-11eb-8146-81bf67d66de4.png",
+      "github": "https://link-dyscovery.herokuapp.com/",
+      "website": "https://github.com/AshlynMarie22/link-dyscovery"
     },
     {
         "id": 2,
+        "name": "Work Day Scheduler",
+        "image": "https://ashlynimages.s3.us-east-2.amazonaws.com/Screen+Shot+2020-09-12+at+2.13.30+PM.png",
+        "github": "https://github.com/AshlynMarie22/Work_Day_Scheduler",
+        "website": "https://ashlynmarie22.github.io/Work_Day_Scheduler/"
+    },
+    {
+        "id": 3,
+        "name": "Project Two: Atmospheric Ghost Adventure",
+        "image": "https://github.com/AshlynMarie22/atmospheric-ghost-adventure/blob/main/public/assets/images/cryptic_encounters_screenshot.png?raw=true",
+        "github": "https://github.com/AshlynMarie22/atmospheric-ghost-adventure",
+        "website": "https://git.heroku.com/atmospheric-ghost-adventure.git"
+    },
+    {
+        "id": 4,
+        "name": "Project One: The Date Night Planner",
+        "image": "https://ashlynimages.s3.us-east-2.amazonaws.com/Screen+Shot+2020-09-18+at+11.40.13+PM.png",
+        "github": "https://github.com/AshlynMarie22/The-Date-Night-Planner.git",
+        "website": "https://ashlynmarie22.github.io/The-Date-Night-Planner/"
+    },
+    {
+        "id": 5,
         "name": "Weather Dashboard",
         "image": "https://ashlynimages.s3.us-east-2.amazonaws.com/Screen+Shot+2020-09-18+at+11.46.40+PM.png",
         "github": "https://github.com/AshlynMarie22/Weather_Dashboard.git",
         "website": "https://ashlynmarie22.github.io/Weather_Dashboard/"
     },
     {
-        "id": 3,
-        "name": "Password Generator",
-        "image": "https://ashlynimages.s3.us-east-2.amazonaws.com/Screen+Shot+2020-09-18+at+11.43.45+PM.png",
-        "github": "https://github.com/AshlynMarie22/Password_Generator.git",
-        "website": "https://ashlynmarie22.github.io/Password_Generator/"
+        "id": 6,
+        "name": "Workout Tracker",
+        "image": "https://ashlynimages.s3.us-east-2.amazonaws.com/Main+Page.png",
+        "github": "https://github.com/AshlynMarie22/Workout_Tracker",
+        "website": "https://workout-tracker-10-22-20.herokuapp.com/"
     },
-    // {
-    //     "id": 4,
-    //     "name": "",
-    //     "image": "",
-    //     "github": "",
-    //     "website": ""
-    // },
-    // {
-    //     "id": 5,
-    //     "name": "",
-    //     "image": "",
-    //     "github": "",
-    //     "website": ""
-    // },
-    // {
-    //     "id": 6,
-    //     "name": "",
-    //     "image": "",
-    //     "github": "",
-    //     "website": ""
-    // },
     // {
     //     "id": 7,
     //     "name": "",
